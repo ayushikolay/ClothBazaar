574be5f552992e5af20aa5e4e7bc5bdb77939bfc
Ayushi Kolay <ayushi18071998@gmail.com>
2019-07-04 01:49:30 +0530

    Added product and category business entities and created databases

---
+++ ClothBazaar.Database/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ClothBazaar.Entities;
+
+namespace ClothBazaar.Database
+{
+    class CBContext: DbContext
+    {
+        public CBContext (): base("ClothBazaarConnection")
+        {
+            }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ClothBazaar.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazaar.Entities
+{
+    public class Product:BaseEntity
+    {
+        public decimal Price { get; set; }
+
+        public Category Category { get; set; }
+        
+        
+    }
+}
